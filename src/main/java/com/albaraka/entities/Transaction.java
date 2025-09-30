319db556344b57bd20b10fa93b749a559c6b3340
belal-allala <belalallala810@gmail.com>
2025-09-30 14:31:21 +0100

    feat: Add Transaction entity record

--- src/main/java/com/albaraka/entities/Transaction.java
+++ src/main/java/com/albaraka/entities/Transaction.java
@@ -1,1 +1,15 @@
 package com.albaraka.entities;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record Transaction(
+        int id,
+        LocalDateTime date,
+        BigDecimal montant,
+        TypeTransaction type,
+        String lieu,
+        int idCompte
+) {
+}
