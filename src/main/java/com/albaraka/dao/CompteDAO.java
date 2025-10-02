8344a3ab206f4d530d890d837a8fe6e55f698d66
belal-allala <belalallala810@gmail.com>
2025-10-02 14:53:53 +0100

    feat: Define CompteDAO interface

--- src/main/java/com/albaraka/dao/CompteDAO.java
+++ src/main/java/com/albaraka/dao/CompteDAO.java
@@ -1,1 +1,14 @@
 package com.albaraka.dao;
+
+import com.albaraka.entities.*;
+import java.util.List;
+import java.util.Optional;  
+
+public interface CompteDAO {
+    Compte save(Compte compte);
+    Optional<Compte> findById(int id);
+    List<Compte> findAll();
+    List<Compte> findByClientId(int clientId);
+    Compte update(Compte compte);
+    void deleteById(int id);
+}
