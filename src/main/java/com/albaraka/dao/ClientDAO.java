4e251ce27f7c6c1320a18bf8bf2eb4955777292b
belal-allala <belalallala810@gmail.com>
2025-10-02 14:53:24 +0100

    feat: Define ClientDAO interface

--- src/main/java/com/albaraka/dao/ClientDAO.java
+++ src/main/java/com/albaraka/dao/ClientDAO.java
@@ -1,1 +1,13 @@
 package com.albaraka.dao;
+
+import com.albaraka.entities.*;
+import java.util.List;
+import java.util.Optional;
+
+public interface ClientDAO {
+    Client save(Client client);
+    Optional<Client> findById(int id);
+    List<Client> findAll();
+    Client update(Client client);
+    void deleteById(int id);
+}
