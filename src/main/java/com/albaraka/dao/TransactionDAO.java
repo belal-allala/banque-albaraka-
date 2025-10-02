e32b05a8070b45aad757d19fa11b94a7409de9c2
belal-allala <belalallala810@gmail.com>
2025-10-02 14:54:22 +0100

    feat: Define TransactionDAO interface

--- src/main/java/com/albaraka/dao/TransactionDAO.java
+++ src/main/java/com/albaraka/dao/TransactionDAO.java
@@ -1,1 +1,14 @@
 package com.albaraka.dao;
+
+import com.albaraka.entities.*;
+import java.util.List;
+import java.util.Optional;
+
+public interface TransactionDAO {
+    Transaction save(Transaction transaction);
+    Optional<Transaction> findById(int id);
+    List<Transaction> findAll();
+    List<Transaction> findByCompteId(int compteId);
+    Transaction update(Transaction transaction);
+    void deleteById(int id);
+}
