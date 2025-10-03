39a1f39182419ca6eab9453a0dc4650b65c53fd9
belal-allala <belalallala810@gmail.com>
2025-10-03 11:42:34 +0100

    feat: Add ClientService for client management logic

--- src/main/java/com/albaraka/services/ClientService.java
+++ src/main/java/com/albaraka/services/ClientService.java
@@ -1,1 +1,34 @@
 package com.albaraka.services;
+
+import com.albaraka.dao.ClientDAO;
+import com.albaraka.entities.Client;
+import java.util.List;
+import java.util.Optional;
+
+public class ClientService {
+    private final ClientDAO clientDAO;
+
+    public ClientService(ClientDAO clientDAO) {
+        this.clientDAO = clientDAO;
+    }
+
+    public Client createClient(Client client) {
+        return clientDAO.save(client);
+    }
+
+    public Optional<Client> getClientById(int id) {
+        return clientDAO.findById(id);
+    }
+
+    public List<Client> getAllClients() {
+        return clientDAO.findAll();
+    }
+
+    public Client updateClient(Client client) {
+        return clientDAO.update(client);
+    }
+
+    public boolean deleteClient(int id) {
+        return clientDAO.delete(id);
+    }
+}
